427ee94b7c0e419ca2d65f100e14039b31783c82
Vladislav Meledin <dmd.vladislav.meledin@vwfspay.com>
2022-08-10 14:30:29 +0700

    Added webpack

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../assets/sass/index.scss';
-import App from './App';
+import App from './app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+
+const plugins = [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'  
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+];
+
+module.exports = {
+    target: env === "production" ? "browserslist" : "web",
+    plugins,
+    devtool: "source-map",
+    entry: "./src/components/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: '[name].bundle.js',
+        publicPath: "/dist/",
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: true
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "./src")
+        },
+        hot: true
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-typescript', '@babel/preset-react'],
+                }
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                ident: "postcss",
+                                plugins: ['postcss-preset-env']
+                            } 
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+                type: env === 'production' ? 'asset' : 'asset/resource'
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
+};
